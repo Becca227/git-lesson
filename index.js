4b2291b42417247adff71a2db070dc97450e050d
Becca227 <markbecca48@gmail.com>
2025-05-19 10:40:49 -0700

    cahnges

--- index.js
+++ index.js
@@ -73,3 +73,102 @@ function discount(price,discount){
 let finalPrice=discount(2000,3);
 
 console.log(finalPrice);
+
+
+let age=18;
+    age+=5;
+console.log(age);
+console.log(!true);
+console.log(!false)
+
+let x=10; 
+console.log(x>9 && x<20);
+
+let isRaining=true
+let isSunny=false
+
+console.log(!isRaining == isSunny && isRaining || isSunny);
+
+let name="John"
+console.log(name =="John"); 
+
+if (name=="John"){
+    console.log("Hello John");
+}
+
+if (5> 9){
+    console.log("5 is greater than 9");
+} else{
+    console.log("5 is not greater than 9");
+}
+
+let year=17;
+if (year>=18){
+    console.log("You are an adult");
+}else if (year>=16 || year>18){
+    console.log("You are a teenager");
+}
+else{
+    console.log("You are a minor");
+}
+
+let day;
+switch (new Date().getDay()) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+       day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+  }
+  console.log("Today is " + day);
+  
+  let car={
+    name: "Audi",
+    color: "red",
+    model: 2020,
+    weight:"400kg",
+  }
+    console.log(car.model);
+
+   let user={
+    name: "",
+    lastName: "",
+    age: 0,
+    email: "",
+    phone: "",
+    address: "",
+    hobbies: []
+   }
+
+  function userProfileManager(name,lastName,age,email,phone,address,hobbies) {
+    user.name= name;
+    user.lastName= lastName;
+    user.age= age;
+    user.email= email;
+    user.phone= phone;
+    user.address= address;
+    user.hobbies= hobbies;
+
+    return user;
+  }
+  let display = userProfileManager("John", "Doe", 25 , "jonhdoe@gmail.com", "123456789", "123 new way city, New York", ["reading", "traveling"]);
+console.log(display);
+
+let display2 = userProfileManager("Jane", "Mick", 30 ,"janemick@gmail.com", "1234567890", "123 new way city, New York", ["swiming", "diving", "cooking"])
+console.log(display2);
+
